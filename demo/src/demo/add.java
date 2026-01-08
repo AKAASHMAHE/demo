16411b17c6f3f74ed1fc54132f6760eca6f32798
AKAASH M K <161186802+AKAASHMAHE@users.noreply.github.com>
2026-01-08 20:00:28 +0530

    Update add.java

--- demo/src/demo/add.java
+++ demo/src/demo/add.java
@@ -1,10 +1,13 @@
 package demo;
-
+import java.util.*;
 public class add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println(3+4);
+			Scanner ip=new Scanner(System.in);
+			int a=ip.nextInt();
+			int b=ip.nextInt()
+;			System.out.println(a+b);
 	}
 
 }
